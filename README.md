86eecf3f45a7a9e4c7a010c204f0a4e47b1b1865
yoyanni <yanni.alevras@gmail.com>
2022-11-27 13:04:09 +0200

    Update canvas to be more robust

--- README.md
+++ README.md
@@ -23,3 +23,9 @@ I have roughly created the canvas with the divs, it isnt at all how it needs to 
 ## Add eventListener which changes the color
 
 Hovering over the specific pixel will append an "active" class name which will then change the color.
+
+## Update Canvas to be more robust
+
+The number previously used to create the canvas was the total number of squares of the canvas. Now it is the number of rows and columns of the canvas. When creating the square divs the amount is taken into account and adjust the size of the divs accordingly. You can change the amount from the UI and it re-renders the canvas.
+
+P.S. I do not like how the responsive sizes are actioned by inline css.
--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 // Selecting the canvas using querySlector in contrast to getElementsByClassName (which brings back HTMLCollection)
-const canvas = document.querySelector(".canvas");
+// const canvas = document.querySelector(".canvas");
+// Using getElementById because it brings back a HTMLElement not a normal Element (which does not have the style attribute)
+const canvas = document.getElementById("canvas");
 
-// Loops 16 times to create the divs for the canvas
-for (let i = 0; i < 16; i++) {
-  let div = document.createElement("div");
-  div.className = "pixel";
-  canvas.appendChild(div);
+// Select input for the number lines for the canvas
+const input = document.querySelector("#input");
+
+// EventListener for the number input
+input.addEventListener("change", changeDivs);
+
+function changeDivs(e) {
+  if (canvas.innerHTML != "") {
+    canvas.innerHTML = "";
+    console.log("Cleared");
+  }
+  createDivs(e.target.valueAsNumber);
+}
+
+// When this first runs it will have 16 divs
+function createDivs(num = 10) {
+  numSquared = num * num;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  for (let i = 0; i < numSquared; i++) {
+    let div = document.createElement("div");
+    div.className = "pixel";
+    div.style.width = `${width / num}px`;
+    div.style.height = `${height / num}px`;
+    canvas.appendChild(div);
+  }
+  eventL();
 }
 
 // Select all pixels (live HTMLCollection)
 const pixels = document.getElementsByClassName("pixel");
 
 // Selects specific pixel and appends the "active" class name
-for (let i = 0; i < pixels.length; i++) {
-  pixels[i].addEventListener("mouseover", (e) => {
-    e.target.className += " active";
-  });
+function eventL() {
+  for (let i = 0; i < pixels.length; i++) {
+    pixels[i].addEventListener("mouseover", (e) => {
+      if (!pixels[i].classList.contains("active")) {
+        e.target.className += " active";
+      }
+    });
+  }
 }
+
+createDivs();
